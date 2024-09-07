ec6ba55a498347fd35eb62547e5191ebefa00f45
Antonin-crypto <akamis53@gmail.com>
2024-09-07 14:40:49 +0200

    branch for test

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,10 @@ app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
