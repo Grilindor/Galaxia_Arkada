c6c476dd4d1a3aba12212c031d8e5048fe3ea2d0
Antonin-crypto <akamis53@gmail.com>
2024-09-10 13:29:49 +0200

    update the Backend-Frontend dockerfile for the node.js version and the server.js file rout and test them

--- Backend/server.js
+++ Backend/server.js
@@ -5,12 +5,24 @@ const app = express();
 app.use(express.json());
 
 // Exemple d'API
-app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+app.get('/home', (req, res) => {
+  res.send('Hello from the backend! ');
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/game', (req, res) => {
+  res.send('Hello World! marc il va dormir ce .......!');
+});
+
+app.get('/login', (req, res) => {
+  res.send('login');
+});
+
+app.get('/review', (req, res) => {
+  res.send('review');
+});
+
+app.get('/Library', (req, res) => {
+  res.send('Library');
 });
 
 const PORT = process.env.PORT || 5000;
