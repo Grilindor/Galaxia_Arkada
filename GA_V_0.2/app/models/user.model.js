d8570379ef67a1bca3c9b584e2c24aae2f643efd
Grilindor <marcor91@hotmail.fr>
2024-10-01 10:03:18 +0200

    add model user

---
+++ GA_V_0.2/app/models/user.model.js
@@ -0,0 +1,34 @@
+const { type } = require("os");
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    firstname: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastname: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    userpsedo: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  });
+
+  return User;
+}
